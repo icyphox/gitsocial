c416b4c1503d7bda68b0e72a0e4481fdf4dfe246
icyphox <icyph0x@protonmail.com>
2018-03-23 18:42:18 +0530

    feat: create peerlist.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import redis
+app = Flask(__name__)
+
+redis = redis.Redis()
+
+@app.route('/api/set')
+def write_redis():
+    user_hash = request.args.get('hash')
+    ip = request.args.get('ip')
+    commit = request.args.get('tip')
+    redis.set(user_hash, set([ip, tip]))
+
+@app.route('/api/get')
+def read_redis():
+    user_hash = request.args.get('hash')
+    return jsonify(redis.get(user_hash))
